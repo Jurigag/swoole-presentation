ea1b4a638a269bc104cc0e8ca2ee14d95d764366
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 16:55:14 +0200

    Use go

--- swoole_mysql_example.php
+++ swoole_mysql_example.php
@@ -10,40 +10,44 @@ $http->set([
 ]);
 
 $channel = new \Swoole\Coroutine\Channel(1);
+$channel2 = new \Swoole\Coroutine\Channel(1);
 
-    go(function () use ($channel) {
-        $pool = new \Smf\ConnectionPool\ConnectionPool([
-            'minActive' => 1,
-            'maxActive' => 18, // workes * maxActive
-            'maxWaitTime' => 60,
-            'maxIdleTime' => 20,
-            'idleCheckInterval' => 10,
-        ],
-            new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
-            [
-                'host' => '127.0.0.1',
-                'port' => '3306',
-                'user' => 'root',
-                'password' => 'indahash',
-                'database' => 'test',
-                'timeout' => 10,
-                'charset' => 'utf8',
-            ]
-        );
-        $pool->init();
+go(function () use ($channel) {
+    $pool = new \Smf\ConnectionPool\ConnectionPool([
+        'minActive' => 1,
+        'maxActive' => 18, // workes * maxActive
+        'maxWaitTime' => 60,
+        'maxIdleTime' => 20,
+        'idleCheckInterval' => 10,
+    ],
+        new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
+        [
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => 'indahash',
+            'database' => 'test',
+            'timeout' => 10,
+            'charset' => 'utf8',
+        ]
+    );
+    $pool->init();
+    $channel->push($pool);
+});
+
+$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use ($channel, $channel2) {
+    go(function () use ($channel, $response, $channel2) {
+        $pool = $channel->pop();
         $mysql = $pool->borrow();
         defer(function () use ($pool, $mysql) {
-            echo "Returning the connection to pool\n";
             $pool->return($mysql);
         });
-        $channel->push($mysql);
-    });
-
-$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use($channel) {
-    go(function() use ($channel, $response) {
-        $mysql = $channel->pop();
         $result = $mysql->query('SELECT * from users');
-        $response->end($result);
+        $channel2->push($result);
+    });
+    go(function () use ($channel2, $response) {
+        $result = $channel2->pop();
+        $response->end(json_encode($result));
     });
 });
 
