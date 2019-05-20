699fbdd2524acd7b76f03b3422a3a9e35d367b76
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 17:00:06 +0200

    Use class

--- swoole_mysql_example.php
+++ swoole_mysql_example.php
@@ -1,55 +1,102 @@
 <?php
 
+use Smf\ConnectionPool\ConnectionPool;
+use Smf\ConnectionPool\ConnectionPoolTrait;
+use Smf\ConnectionPool\Connectors\CoroutineMySQLConnector;
+use Smf\ConnectionPool\Connectors\PhpRedisConnector;
+use Swoole\Coroutine\MySQL;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
 use Swoole\Http\Server as ServerAlias;
+use Swoole\Http\Server;
 
 require 'vendor/autoload.php';
 
-$http = new ServerAlias("0.0.0.0", 80);
-$http->set([
-    'worker_num' => 8
-]);
+class HttpServer
+{
+    use ConnectionPoolTrait;
 
-$channel = new \Swoole\Coroutine\Channel(1);
-$channel2 = new \Swoole\Coroutine\Channel(1);
+    protected $swoole;
 
-go(function () use ($channel) {
-    $pool = new \Smf\ConnectionPool\ConnectionPool([
-        'minActive' => 1,
-        'maxActive' => 18, // workes * maxActive
-        'maxWaitTime' => 60,
-        'maxIdleTime' => 20,
-        'idleCheckInterval' => 10,
-    ],
-        new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
-        [
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'user' => 'root',
-            'password' => 'indahash',
-            'database' => 'test',
-            'timeout' => 10,
-            'charset' => 'utf8',
-        ]
-    );
-    $pool->init();
-    $channel->push($pool);
-});
+    public function __construct(string $host, int $port)
+    {
+        $this->swoole = new Server($host, $port);
 
-$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use ($channel, $channel2) {
-    go(function () use ($channel, $response, $channel2) {
-        $pool = $channel->pop();
-        $mysql = $pool->borrow();
-        defer(function () use ($pool, $mysql) {
-            $pool->return($mysql);
+        $this->setDefault();
+        $this->bindWorkerEvents();
+        $this->bindHttpEvent();
+    }
+
+    protected function setDefault()
+    {
+        $this->swoole->set([
+            'daemonize'             => false,
+            'dispatch_mode'         => 1,
+            'max_request'           => 8000,
+            'open_tcp_nodelay'      => true,
+            'reload_async'          => true,
+            'max_wait_time'         => 60,
+            'enable_reuse_port'     => true,
+            'enable_coroutine'      => true,
+            'http_compression'      => false,
+            'enable_static_handler' => false,
+            'buffer_output_size'    => 4 * 1024 * 1024,
+            'worker_num'            => 4, // Each worker holds a connection pool
+        ]);
+    }
+
+    protected function bindHttpEvent()
+    {
+        $this->swoole->on('Request', function (Request $request, Response $response) {
+            $pool1 = $this->getConnectionPool('mysql');
+            /**@var MySQL $mysql */
+            $mysql = $pool1->borrow();
+            defer(function () use ($pool1, $mysql) {
+                $pool1->return($mysql);
+            });
+            $result = $mysql->query('SELECT * FROM users');
+            $response->header('Content-Type', 'application/json');
+            $response->end(json_encode($result));
         });
-        $result = $mysql->query('SELECT * from users');
-        $channel2->push($result);
-    });
-    go(function () use ($channel2, $response) {
-        $result = $channel2->pop();
-        $response->end(json_encode($result));
-    });
-});
+    }
 
-$http->start();
+    protected function bindWorkerEvents()
+    {
+        $createPools = function () {
+            // All MySQL connections: [4 workers * 2 = 8, 4 workers * 10 = 40]
+            $pool1 = new ConnectionPool(
+                [
+                    'minActive' => 2,
+                    'maxActive' => 15,
+                ],
+                new CoroutineMySQLConnector,
+                [
+                    'host'        => '127.0.0.1',
+                    'port'        => '3306',
+                    'user'        => 'root',
+                    'password'    => 'indahash',
+                    'database'    => 'test',
+                    'timeout'     => 10,
+                    'charset'     => 'utf8',
+                    'strict_type' => true,
+                    'fetch_mode'  => true,
+                ]);
+            $pool1->init();
+            $this->addConnectionPool('mysql', $pool1);
+        };
+        $closePools = function () {
+            $this->closeConnectionPools();
+        };
+        $this->swoole->on('WorkerStart', $createPools);
+        $this->swoole->on('WorkerStop', $closePools);
+        $this->swoole->on('WorkerError', $closePools);
+    }
 
+    public function start()
+    {
+        $this->swoole->start();
+    }
+}
+
+$server = new HttpServer('0.0.0.0', 80);
+$server->start();
