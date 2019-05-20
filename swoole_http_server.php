a7209e3612f2260cf08a52d39388c4c79f6313f7
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 13:06:42 +0200

    Add php server

--- swoole_http_server.php
+++ swoole_http_server.php
@@ -5,7 +5,8 @@ use Swoole\Http\Server as ServerAlias;
 $http = new ServerAlias("0.0.0.0", 80);
 
 $http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
-    $response->end("<h1>Hello World. #".rand(1000, 9999)."</h1>");
+    $response->header("Content-Type", "text/plain");
+    $response->end("<h1>Hello World</h1>");
 });
 
 $http->start();
