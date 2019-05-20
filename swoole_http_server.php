cd8fe8d257973f693210ec9075064557ae1efb7f
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 13:23:50 +0200

    8 worker num

--- swoole_http_server.php
+++ swoole_http_server.php
@@ -3,6 +3,9 @@
 use Swoole\Http\Server as ServerAlias;
 
 $http = new ServerAlias("0.0.0.0", 80);
+$http->set([
+    'worker_num' => 8
+]);
 
 $http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
     $response->header("Content-Type", "text/plain");
