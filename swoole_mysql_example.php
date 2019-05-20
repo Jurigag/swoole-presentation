849307df6d4c0851b5b2d4c220ce43048d6763d3
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 16:37:42 +0200

    Use go

--- swoole_mysql_example.php
+++ swoole_mysql_example.php
@@ -9,33 +9,43 @@ $http->set([
     'worker_num' => 8
 ]);
 
-$pool = new \Smf\ConnectionPool\ConnectionPool([
-    'minActive'         => 1,
-    'maxActive'         => 18, // workes * maxActive
-    'maxWaitTime'       => 60,
-    'maxIdleTime'       => 20,
-    'idleCheckInterval' => 10,
-],
-    new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
-    [
-        'host'        => '127.0.0.1',
-        'port'        => '3306',
-        'user'        => 'root',
-        'password'    => 'indahash',
-        'database'    => 'test',
-        'timeout'     => 10,
-        'charset'     => 'utf8',
-        'strict_type' => true,
-        'fetch_mode'  => true,
-    ]
-);
-$pool->init();
+$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
+    $channel = new \Swoole\Coroutine\Channel(1);
+    go(function() use($channel) {
 
-$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use($pool) {
-    $mysql = $pool->borrow();
-    $result = $mysql->query('select * FROM users');
-    $pool->return($mysql);
-    $response->end(json_encode($result));
+        $pool = new \Smf\ConnectionPool\ConnectionPool([
+            'minActive'         => 1,
+            'maxActive'         => 18, // workes * maxActive
+            'maxWaitTime'       => 60,
+            'maxIdleTime'       => 20,
+            'idleCheckInterval' => 10,
+        ],
+            new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
+            [
+                'host'        => '127.0.0.1',
+                'port'        => '3306',
+                'user'        => 'root',
+                'password'    => 'indahash',
+                'database'    => 'test',
+                'timeout'     => 10,
+                'charset'     => 'utf8',
+            ]
+        );
+        $pool->init();
+        defer(function () use ($pool) {
+            echo "Closing connection pool\n";
+            $pool->close();
+        });
+        $mysql = $pool->borrow();
+        defer(function () use ($pool) {
+            $pool->close();
+        });
+        $result = $mysql->query('select * FROM users');
+        $channel->push($result);
+    });
+    go(function() use ($channel, $response) {
+        $response->end(json_encode($channel->pop()));
+    });
 });
 
 $http->start();
