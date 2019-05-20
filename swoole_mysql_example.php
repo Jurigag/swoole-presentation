5e6e1d54122b29fba6766ba2293cf2f3f8f49801
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 15:54:28 +0200

    Add connection pool

--- swoole_mysql_example.php
+++ swoole_mysql_example.php
@@ -7,18 +7,33 @@ $http->set([
     'worker_num' => 8
 ]);
 
-$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
-    $mysql = new Swoole\Coroutine\MySQL();
-    $mysql->connect([
-        'host' => '127.0.0.1',
-        'user' => 'root',
-        'password' => 'indahash',
-        'database' => 'test',
-    ]);
-    $mysql->setDefer();
-    $mysql->query('select * FROM users');
-    $mysql_response = $mysql->recv();
-    $response->end(json_encode($mysql_response));
+$pool = new \Smf\ConnectionPool\ConnectionPool([
+    'minActive'         => 1,
+    'maxActive'         => 18, // workes * maxActive
+    'maxWaitTime'       => 60,
+    'maxIdleTime'       => 20,
+    'idleCheckInterval' => 10,
+],
+    new \Smf\ConnectionPool\Connectors\CoroutineMySQLConnector(),
+    [
+        'host'        => '127.0.0.1',
+        'port'        => '3306',
+        'user'        => 'root',
+        'password'    => 'indahash',
+        'database'    => 'test',
+        'timeout'     => 10,
+        'charset'     => 'utf8',
+        'strict_type' => true,
+        'fetch_mode'  => true,
+    ]
+);
+$pool->init();
+
+$http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use($pool) {
+    $mysql = $pool->borrow();
+    $result = $mysql->query('select * FROM users');
+    $pool->return($mysql);
+    $response->end(json_encode($result));
 });
 
 $http->start();
