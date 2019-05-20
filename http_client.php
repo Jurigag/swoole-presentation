02aead2de6bd519610f453d5c4594f791e7c360b
Wojciech Ślawski <wojciech.slawski@indahash.com>
2019-05-20 11:42:08 +0200

    Change ip

--- http_client.php
+++ http_client.php
@@ -2,7 +2,7 @@
 
 use Swoole\Http\Server as ServerAlias;
 
-$http = new ServerAlias("127.0.0.1", 80);
+$http = new ServerAlias("0.0.0.0", 80);
 
 $http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
     $response->end("<h1>Hello World. #".rand(1000, 9999)."</h1>");
