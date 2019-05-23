0e43f1dd16721c132cba45e575006735e7f0a85f
Wojciech Ślawski <jurigag@gmail.com>
2019-05-24 00:02:11 +0200

    More requests

--- swoole_chan_client_example.php
+++ swoole_chan_client_example.php
@@ -1,10 +1,12 @@
 <?php
 
-$chan = new chan(2);
+use Swoole\Coroutine\Http\Client;
+
+$chan = new chan(6);
 
 go(function () use ($chan) {
     $result = [];
-    for ($i = 0; $i < 2; $i++)
+    for ($i = 0; $i < 6; $i++)
     {
         $result[] = $chan->pop();
     }
@@ -28,6 +30,58 @@ go(function () use ($chan) {
 // Start fetching the second webpage without blocking the script
 go(function () use ($chan) {
     $cli = new Swoole\Coroutine\Http\Client('www.bing.com', 80);
+    $cli->set(['timeout' => 10]);
+    $cli->setHeaders([
+        'Host' => "www.bing.com",
+        "User-Agent" => 'Chrome/49.0.2587.3',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Encoding' => 'gzip',
+    ]);
+    $ret = $cli->get('/');
+    $chan->push(['www.bing.com' => $cli->statusCode]);
+});
+
+go(function () use ($chan) {
+    $cli = new Swoole\Coroutine\Http\Client('www.indahash.com', 80);
+    $cli->set(['timeout' => 10]);
+    $cli->setHeaders([
+        'Host' => "www.bing.com",
+        "User-Agent" => 'Chrome/49.0.2587.3',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Encoding' => 'gzip',
+    ]);
+    $ret = $cli->get('/');
+    $chan->push(['www.bing.com' => $cli->statusCode]);
+});
+
+go(function () use ($chan) {
+    $cli = new Swoole\Coroutine\Http\Client('www.wykop.pl', 80);
+    $cli->set(['timeout' => 10]);
+    $cli->setHeaders([
+        'Host' => "www.bing.com",
+        "User-Agent" => 'Chrome/49.0.2587.3',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Encoding' => 'gzip',
+    ]);
+    $ret = $cli->get('/');
+    $chan->push(['www.bing.com' => $cli->statusCode]);
+});
+
+go(function () use ($chan) {
+    $cli = new Swoole\Coroutine\Http\Client('www.facebook.com', 80);
+    $cli->set(['timeout' => 10]);
+    $cli->setHeaders([
+        'Host' => "www.bing.com",
+        "User-Agent" => 'Chrome/49.0.2587.3',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Encoding' => 'gzip',
+    ]);
+    $ret = $cli->get('/');
+    $chan->push(['www.bing.com' => $cli->statusCode]);
+});
+
+go(function () use ($chan) {
+    $cli = new Swoole\Coroutine\Http\Client('www.onet.pl', 80);
     $cli->set(['timeout' => 10]);
     $cli->setHeaders([
         'Host' => "www.bing.com",
