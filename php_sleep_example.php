da4ff00861c84366bee178929df14cb0e93f4f2e
Wojciech Ślawski <jurigag@gmail.com>
2019-05-23 19:46:01 +0200

    Add sleep examples code

--- php_sleep_example.php
+++ php_sleep_example.php
@@ -1,1 +1,16 @@
 <?php
+
+function sleep1()
+{
+    echo 'a';
+    sleep(1);
+}
+
+function sleep2()
+{
+    echo 'b';
+    sleep(2);
+}
+
+sleep1();
+sleep2();
--- swoole_sleep_example.php
+++ swoole_sleep_example.php
@@ -1,1 +1,11 @@
 <?php
+
+\Swoole\Runtime::enableCoroutine();
+go(function(){
+    echo 'a';
+    sleep(1);
+});
+go(function(){
+    echo 'b';
+    sleep(2);
+});
