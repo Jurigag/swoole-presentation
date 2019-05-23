2a1280217f49de79dc7f5933fb0ef7a95ed42bca
Wojciech Ślawski <jurigag@gmail.com>
2019-05-23 23:59:14 +0200

    Remove setopt

--- php_client_example.php
+++ php_client_example.php
@@ -3,9 +3,6 @@
 function request($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, TRUE);
-    curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         'Host' => "www.google.com",
         "User-Agent" => 'Chrome/49.0.2587.3',
