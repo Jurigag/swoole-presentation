64ba6f37aa89714236b01fbb8d2514acc8e5fe06
Wojciech Ślawski <jurigag@gmail.com>
2019-05-23 20:04:17 +0200

    Add defer example

--- swoole_defer_example.php
+++ swoole_defer_example.php
@@ -1,1 +1,18 @@
 <?php
+
+Swoole\Runtime::enableCoroutine();
+
+go(function () {
+    echo "a";
+    defer(function () {
+        sleep(2);
+        echo "~a";
+    });
+    echo "b";
+    defer(function () {
+        sleep(1);
+        echo "~b";
+    });
+    sleep(1);
+    echo "c";
+});
